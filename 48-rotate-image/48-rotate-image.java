cdd466b8b379100957c925ffe1fccf25d195858e
Vasu <vasuratanpara@gmail.com>
2022-07-19 22:28:39 -0400

    Time: 0 ms (100.00%), Space: 42.6 MB (69.57%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        int left = 0;
+        int right = matrix.length-1;
+        
+        
+        while(left < right) {
+            
+            for(int i = 0; i < (right-left); i++){
+                
+                // Store A in the temp
+                int temp = matrix[left][left+i];
+                
+                // Update A with B
+                matrix[left][left+i] = matrix[right-i][left];
+
+                // Update B with C
+                matrix[right-i][left] = matrix[right][right-i];
+                
+                // Update C with D
+                matrix[right][right-i] = matrix[left+i][right];
+                
+                // Update D with A
+                matrix[left+i][right] = temp;
+            }
+            
+            right--;
+            left++;
+            
+        }
+    }
+}
