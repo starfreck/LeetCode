6be73f45da809a68304ad42e1dafa2c4ec139d01
Vasu <vasuratanpara@gmail.com>
2024-11-27 18:57:28 -0500

    Time: 23 ms (16.78%), Space: 31.2 MB (48%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,26 @@
+# TC: O(n)
+# SC: O(n)
+
+class Solution:
+
+    """
+    TC: O(1)
+    SC: O(n) for the set of size n
+    """
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(set(nums)) == len(nums):
+            return False
+        return True
+
+    """
+    TC: O(n)
+    SC: O(n)
+    """
+    def containsDuplicateS1(self, nums: List[int]) -> bool:
+        s = set() # O(n)
+        for num in nums: # O(n)
+            if num in s: # O(1)
+                return True
+            else:
+                s.add(num) 
+        return False
